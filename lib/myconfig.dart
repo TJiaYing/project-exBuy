12b2c84cec94651739f69055cca647c56d54b489
Toh Jia Ying <121545315+TJiaYing@users.noreply.github.com>
2023-07-03 18:59:26 +0800

    Add files via upload

---
+++ lib/myconfig.dart
@@ -0,0 +1,3 @@
+class MyConfig {
+  String SERVER = "http://192.168.215.73/exbuydb";
+}
---
+++ lib/screens/buyerscreen.dart
@@ -0,0 +1,276 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../models/catch.dart';
+import '../models/user.dart';
+import '../myconfig.dart';
+import 'buyercartscreen.dart';
+import 'buyerdetialscreen.dart';
+
+//for buyer screen
+
+class BuyerScreen extends StatefulWidget {
+  final User user;
+  const BuyerScreen({super.key, required this.user});
+
+  @override
+  State<BuyerScreen> createState() => _BuyerScreenState();
+}
+
+class _BuyerScreenState extends State<BuyerScreen> {
+  String maintitle = "Buyer";
+  List<Catch> catchList = <Catch>[];
+  late double screenHeight, screenWidth;
+  late int axiscount = 2;
+  int numofpage = 1, curpage = 1;
+  int numberofresult = 0;
+  var color;
+  int cartqty = 0;
+
+  TextEditingController searchController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    loadCatches(1);
+    print("Buyer");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth > 600) {
+      axiscount = 3;
+    } else {
+      axiscount = 2;
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(maintitle),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showsearchDialog();
+              },
+              icon: const Icon(Icons.search)),
+              TextButton.icon(
+            icon: const Icon(
+              Icons.shopping_cart,
+            ), // Your icon here
+            label: Text(cartqty.toString()), // Your text here
+            onPressed: () {
+              if (cartqty > 0) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (content) => BuyerCartScreen(
+                              user: widget.user,
+                            )));
+              }else{
+                 ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("No item in cart")));
+              }
+            },
+              )
+        ],
+      ),
+      body: catchList.isEmpty
+          ? const Center(
+              child: Text("No Data"),
+            )
+          : Column(children: [
+              Container(
+                height: 24,
+                color: Theme.of(context).colorScheme.primary,
+                alignment: Alignment.center,
+                child: Text(
+                  "$numberofresult Items Found",
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+              Expanded(
+                  child: GridView.count(
+                      crossAxisCount: axiscount,
+                      children: List.generate(
+                        catchList.length,
+                        (index) {
+                          return Card(
+                            child: InkWell(
+                              onTap: () async {
+                                Catch usercatch =
+                                    Catch.fromJson(catchList[index].toJson());
+                                await Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (content) =>
+                                            BuyerDetailsScreen(
+                                              user: widget.user,
+                                              usercatch: usercatch,
+                                            )));
+                                            loadCatches(1);
+                              },
+                              child: Column(children: [
+                                CachedNetworkImage(
+                                  width: screenWidth,
+                                  fit: BoxFit.cover,
+                                  imageUrl:
+                                      "${MyConfig().SERVER}/assets/catches/${catchList[index].catchId}.png",
+                                  placeholder: (context, url) =>
+                                      const LinearProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                ),
+                                Text(
+                                  catchList[index].catchName.toString(),
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                                Text(
+                                  "RM ${double.parse(catchList[index].catchPrice.toString()).toStringAsFixed(2)}",
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  "${catchList[index].catchQty} available",
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                              ]),
+                            ),
+                          );
+                        },
+                      ))),
+                      SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: numofpage,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    //build the list for textbutton with scroll
+                    if ((curpage - 1) == index) {
+                      //set current page number active
+                      color = Colors.red;
+                    } else {
+                      color = Colors.black;
+                    }
+                    return TextButton(
+                        onPressed: () {
+                          curpage = index + 1;
+                          loadCatches(index + 1);
+                        },
+                        child: Text(
+                          (index + 1).toString(),
+                          style: TextStyle(color: color, fontSize: 18),
+                        ));
+                  },
+                ),
+              ),
+            ]),
+    );
+  }
+
+  void loadCatches(int pg) {
+    http.post(Uri.parse("${MyConfig().SERVER}/php/load_catches.php"),
+        body: {
+          "cartuserid": widget.user.id,
+          "pageno": pg.toString()
+        }).then((response) {
+      //print(response.body);
+      log(response.body);
+      catchList.clear();
+      if (response.statusCode == 200) {
+        var jsondata = jsonDecode(response.body);
+        if (jsondata['status'] == "success") {
+          numofpage = int.parse(jsondata['numofpage']); //get number of pages
+          numberofresult = int.parse(jsondata['numberofresult']);
+          print(numberofresult);
+          var extractdata = jsondata['data'];
+          cartqty = int.parse(jsondata['cartqty'].toString());
+          print(cartqty);
+          extractdata['catches'].forEach((v) {
+            catchList.add(Catch.fromJson(v));
+          });
+          print(catchList[0].catchName);
+        }
+        setState(() {});
+      }
+    });
+  }
+
+  void showsearchDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: const Text(
+            "Search?",
+            style: TextStyle(),
+          ),
+          content: Column(mainAxisSize: MainAxisSize.min, children: [
+            TextField(
+                controller: searchController,
+                decoration: const InputDecoration(
+                    labelText: 'Search',
+                    labelStyle: TextStyle(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2.0),
+                    ))),
+            const SizedBox(
+              height: 4,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  String search = searchController.text;
+                  searchCatch(search);
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Search"))
+          ]),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                "Close",
+                style: TextStyle(),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void searchCatch(String search) {
+    http.post(Uri.parse("${MyConfig().SERVER}/php/load_catches.php"),
+        body: {
+          "cartuserid": widget.user.id,
+          "search": search
+          }).then((response) {
+      //print(response.body);
+      log(response.body);
+      catchList.clear();
+      if (response.statusCode == 200) {
+        var jsondata = jsonDecode(response.body);
+        if (jsondata['status'] == "success") {
+          var extractdata = jsondata['data'];
+          extractdata['catches'].forEach((v) {
+            catchList.add(Catch.fromJson(v));
+          });
+          print(catchList[0].catchName);
+        }
+        setState(() {});
+      }
+    });
+  }
+}
