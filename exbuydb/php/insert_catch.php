4c1be50b0287af7df170f9e2a38682ccde1a7fed
Toh Jia Ying <121545315+TJiaYing@users.noreply.github.com>
2023-07-03 18:58:47 +0800

    Add files via upload

--- exbuydb/php/insert_catch.php
+++ exbuydb/php/insert_catch.php
@@ -18,8 +18,9 @@ $longitude = $_POST['longitude'];
 $state = $_POST['state'];
 $locality = $_POST['locality'];
 $image = $_POST['image'];
+$status = "New";
 
-$sqlinsert = "INSERT INTO `tbl_catches`(`user_id`,`catch_name`, `catch_desc`, `catch_type`, `catch_price`, `catch_qty`, `catch_lat`, `catch_long`, `catch_state`, `catch_locality`) VALUES ('$userid','$catch_name','$catch_desc','$catch_type','$catch_price','$catch_qty','$latitude','$longitude','$state','$locality')";
+$sqlinsert = "INSERT INTO `tbl_catches`(`user_id`,`catch_name`, `catch_desc`, `catch_type`, `catch_price`, `catch_qty`, `catch_lat`, `catch_long`, `catch_state`, `catch_locality`, `catch_status`) VALUES ('$userid','$catch_name','$catch_desc','$catch_type','$catch_price','$catch_qty','$latitude','$longitude','$state','$locality','New')";
 
 if ($conn->query($sqlinsert) === TRUE) {
 	$filename = mysqli_insert_id($conn);
