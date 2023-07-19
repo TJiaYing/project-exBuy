cf1ed44b148eb11167ef1a51f7a3953a280c90c9
Toh Jia Ying <121545315+TJiaYing@users.noreply.github.com>
2023-07-19 21:26:19 +0800

    Add files via upload

--- exbuydb/php/payment.php
+++ exbuydb/php/payment.php
@@ -18,10 +18,10 @@ $data = array(
           'email' => $email,
           'mobile' => $phone,
           'name' => $name,
-          'amount' => ($amount) * 100, // RM20
+          'amount' => 200, // RM2
       'description' => 'Payment for order by '.$name,
-          'callback_url' => "http://10.19.24.116/exbuydb/return_url",
-          'redirect_url' => "http://10.19.24.116/exbuydb/php/payment_update.php?userid=$userid&email=$email&phone=$phone&amount=$amount&name=$name" 
+          'callback_url' => "http://jiaying.infinityfreeapp.com/exbuydb/return_url",
+          'redirect_url' => "http://jiaying.infinityfreeapp.com/exbuydb/php/payment_update.php?userid=$userid&email=$email&phone=$phone&amount=$amount&name=$name" 
 );
 
 $process = curl_init($host );
@@ -29,6 +29,8 @@ curl_setopt($process, CURLOPT_HEADER, 0);
 curl_setopt($process, CURLOPT_USERPWD, $api_key . ":");
 curl_setopt($process, CURLOPT_TIMEOUT, 30);
 curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($process, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($process, CURLOPT_POSTFIELDS, http_build_query($data) ); 
 
 $return = curl_exec($process);
